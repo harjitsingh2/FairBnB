5e0e94e90a5621c25ae9b8147084553044e09f4c
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-28 12:25:32 -0400

    Update Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,49 @@ class ApplicationController < ActionController::API
     before_action :attach_authenticity_token
 
     def test
-        render json: { message: ["Hello from Rails"] }
+        # render json: { message: ["Hello from Rails"] }
+
+        if params.has_key?(:login)
+            login(User.first)
+          elsif params.has_key?(:logout)
+            logout
+          end
+        
+          if current_user
+            render json: { user: current_user.slice('id', 'email', 'session_token') }
+          else
+            render json: ['No current user']
+          end
+    end
+
+    def current_user 
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        if !logged_in?
+            render json: { errors: ['Must be logged in'] }, status: :unauthorized
+        end
+    end
+
+    def require_logged_out
+        if logged_in?
+            render json: { errors: ['Must be logged out'] }, status: 403
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
     end
     
     private
