29eb2ec025b89cbcb56b0a19cdfd8c945cdfa640
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-27 20:02:28 -0400

    Create User model, validations, and methods

---
+++ app/models/user.rb
@@ -0,0 +1,62 @@
+class User < ApplicationRecord
+  
+  before_validation :ensure_session_token
+  attr_reader :password 
+
+  # Validations
+  
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :session_token, uniqueness: true, presence: true 
+  validates :password, allow_nil: true, length: { minimum: 8 }
+
+  # Associations
+  has_many :listings, 
+  foreign_key: :host_id,
+  class_name: :Listings,
+  dependent: :destroy
+
+  has_many :reservations,
+  foreign_key: :guest_id,
+  class_name: :Reservations,
+  dependent: :destroy
+
+  has_many :reviews,
+  foreign_key: :reviewer_id,
+  class_name: :Reviews,
+  dependent: :destroy 
+
+  # Methods
+  
+  has_secure_password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user&.authenticate(password) 
+        return user
+    else
+        nil 
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    save!
+    session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    while true
+        token = SecureRandom.urlsafe_base64
+        return token unless User.exists?(session_token: token)
+    end
+  end
+  
+end
