97f636d30ae1c77cfd1ec7665cb4cd7f796a1177
harjitsingh3 <singh.harjit09@gmail.com>
2023-06-02 10:52:55 -0400

    Create Listing Actions and Reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import ui from './ui';
+import listings from './listings';
 
 const rootReducer = combineReducers({
   session,
-  ui
+  ui,
+  listings
 });
 
 let enhancer;
---
+++ frontend/src/store/listings.js
@@ -0,0 +1,51 @@
+import csrfFetch from "./csrf.js";
+
+// Action Constants
+const SET_LISTING = 'listings/setListing';
+const SET_LISTINGS = 'listings/setListings';
+
+// Action Creators
+export const setListing = (listing) => ({
+    type: SET_LISTING,
+    payload: listing
+});
+
+export const setListings = (listings) => ({
+    type: SET_LISTINGS,
+    payload: listings 
+})
+
+// Thunk Action Creators
+export const fetchListing = (listingId) => async dispatch => {
+    const response = await csrfFetch(`/api/listings/${listingId}`);
+
+    const data = await response.json();
+    dispatch(setListing(data.listing));
+    return response;
+}
+
+export const fetchListings = () => async dispatch => {
+    const response = await csrfFetch("/api/listings");
+
+    const data = await response.json();
+    dispatch(setListing(data));
+    return response;
+}
+
+// Listings Reducer
+const listingsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let newState = {...state};
+
+    switch (action.type) {
+        case SET_LISTING:
+            newState[action.listing.id] = action.listing;
+            return newState;
+        case SET_LISTINGS:
+          return {...newState, ...action.listings};
+        default:
+          return state;
+      }
+}
+
+export default listingsReducer;
