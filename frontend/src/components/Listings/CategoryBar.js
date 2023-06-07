ba88850988fd602a62cbbeed88713cbd2b89ded7
harjitsingh3 <singh.harjit09@gmail.com>
2023-06-07 10:49:18 -0400

    Styled category bar more

--- frontend/src/components/Listings/CategoryBar.js
+++ frontend/src/components/Listings/CategoryBar.js
@@ -23,36 +23,44 @@ const CategoryBar = () => {
 
                 <div className="category-icon" id="beachfront" onClick={() => handleCategoryClick('beachfront')}>
                     <img src={beachfront} />
+                    <br></br>
                     <span className="category-name">Beachfront</span>
                 {/* </div> */}
             </div>
             <div className="category-icon" id="cabin" onClick={() => handleCategoryClick('cabin')}>
                 {/* <a href="https://www.flaticon.com/free-icons/cabin" title="cabin icons"><img src={cabin} /></a> */}
                 <img src={cabin} alt=""/>
+                <br></br>
                 <span className="category-name">Cabin</span>
             </div>
             <div className="category-icon" id="camping" onClick={() => handleCategoryClick('camping')}>
                 <img src={camping} alt=""/>
+                <br></br>
                 <span className="category-name">Camping</span>
             </div>
             <div className="category-icon" id="city" onClick={() => handleCategoryClick('city')}>
                 <img src={city} alt=""/>
+                <br></br>
                 <span className="category-name">City</span>
             </div>
             <div className="category-icon" id="countryside" onClick={() => handleCategoryClick('countryside')}>
                 <img src={countryside} alt=""/>
+                <br></br>
                 <span className="category-name">Countryside</span>
             </div>
             <div className="category-icon" id="mansion" onClick={() => handleCategoryClick('mansion')}>
                 <img src={mansion} alt=""/>
+                <br></br>
                 <span className="category-name">Mansion</span>
             </div>
             <div className="category-icon" id="tiny home" onClick={() => handleCategoryClick('tiny home')}>
                 <img src={tinyhome} alt=""/>
+                <br></br>
                 <span className="category-name">Tiny Home</span>
             </div>
             <div className="category-icon" id="treehouse" onClick={() => handleCategoryClick('treehouse')}>
                 <img src={treehouse} alt=""/>
+                <br></br>
                 <span className="category-name">Treehouse</span>
             </div>
             
