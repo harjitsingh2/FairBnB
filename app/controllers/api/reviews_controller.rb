5783f8dbea54d274cfb48fc678506150841c1a38
harjitsingh3 <singh.harjit09@gmail.com>
2023-07-06 14:37:19 -0400

    Create review controller

---
+++ app/controllers/api/reviews_controller.rb
@@ -0,0 +1,66 @@
+class Api::ReviewsController < ApplicationController
+    def index 
+        listing = Listing.find_by(id: review_params[:listing_id])
+
+        @reviews = Review.all.where(listing_id: listing.id)
+        render :index 
+    end
+
+    def show 
+        reservation = Reservation.find_by(id: review_params[:reservation_id])
+
+        @review = Review.find_by(id: params[:id])
+        if @review 
+            render :show 
+        end 
+    end
+
+    def create 
+        reservation = Reservation.find_by(id: review_params[:reservation_id])
+
+        if reservation
+            @review = Review.new(review_params)
+
+            if @review.save 
+                render :show 
+            else 
+                render json: { errors: @review.errors.full_messages }, status: 422
+            end 
+        else 
+            render json: { errors: ["Cannot create a review for a non-existent reservation"]}
+        end 
+    end
+
+    def update 
+        @review = Review.find_by(id: params[:id])
+
+        if @review
+            if (@review.reviewer_id == current_user.id)
+                if @review.update(review_params)
+                    render :show 
+                else 
+                    render json: { errors: @review.errors.full_messages }, status: 422
+                end
+            else 
+                render json: { errors: ["Cannot edit a review that doesn't belong to you"]}, status: 401
+            end 
+        else 
+            render json: { errors: ["The requested review does not exist"] }, status: 404
+        end 
+    end
+
+    def destroy 
+        @review = Review.find(params[:id])
+        if (@review.reviewer_id == current_user.id)
+            @review.destroy 
+            head :no_content
+        else
+            render json: { errors: ["Cannot delete a review that doesn't belong to you"]}, status: 401
+        end
+    end
+
+    private 
+    def review_params 
+        params.require(:review).permit(:listing_id, :reservation_id, :reviewer_id, :rating, :cleanliness, , :communication, :checkin, :accuracy, :location, :value)
+    end
+end
