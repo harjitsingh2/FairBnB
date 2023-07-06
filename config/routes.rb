5faf61de8a0bdc70408509a6e5eafe020bfbb877
harjitsingh3 <singh.harjit09@gmail.com>
2023-07-06 16:02:49 -0400

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :show, :destroy]
     resources :listings, only: [:index, :show]
     resources :reservations, only: [:index, :show, :create, :update, :destroy]
+    resources :reviews, only: [:index, :show, :create, :update, :destroy]
   end 
 
   get '*path', to: "static_pages#frontend_index"
