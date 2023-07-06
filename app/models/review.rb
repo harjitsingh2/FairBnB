53711fc6cb25e323188f898febe901313fe91632
harjitsingh3 <singh.harjit09@gmail.com>
2023-07-06 13:55:13 -0400

    Review model and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,20 @@
 class Review < ApplicationRecord
+    # Validations
+    validates :listing_id, :reservation_id, :reviewer_id, :rating, presence: true
+    validates :cleanliness, , :communication, :checkin, :accuracy, :location, :value, presence: true, inclusion: { in: 1..5 }
+    validate :overall_rating
+
+    # Associations
+    belongs_to :listing,
+        class_name: :Listing,
+        foreign_key: :listing_id 
+    
+    belongs_to :reviewer,
+        class_name: :User,
+        foreign_key: :reviewer_id
+
+    # Methods
+    def overall_rating
+        self.rating = ((cleanliness + communication + checkin + accuracy + location + value) / 6.0).round(2)
+    end
 end
