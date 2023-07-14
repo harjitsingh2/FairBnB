ef5121782e9e9f442d9435625d74ae6c62951278
harjitsingh3 <singh.harjit09@gmail.com>
2023-07-14 12:17:43 -0700

    Review modal closes on submit

--- frontend/src/components/Reviews/ReviewForm.js
+++ frontend/src/components/Reviews/ReviewForm.js
@@ -6,7 +6,7 @@ import { createReview } from '../../store/reviews';
 import './ReviewForm.css';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
-const ReviewsForm = ({ listingId, reservationId }) => {
+const ReviewsForm = ({ listingId, reservationId, closeModal }) => {
   const { register, formState: { errors } } = useForm();
   const dispatch = useDispatch();
 
@@ -22,6 +22,7 @@ const ReviewsForm = ({ listingId, reservationId }) => {
     e.preventDefault();
     const payload = { listingId, reservationId, cleanliness, communication, checkin, accuracy, location, value, body }
     dispatch(createReview(payload));
+    closeModal();
   };
 
 
@@ -108,145 +109,3 @@ const ReviewsForm = ({ listingId, reservationId }) => {
 };
 
 export default ReviewsForm;
-
-
-// import React, {useState} from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useForm } from 'react-hook-form';
-// import { createReview } from '../../store/reviews';
-// // import StarRating from './StarRating';
-// import './ReviewForm.css';
-// import Modal from 'react-modal';
-
-// const ReviewsForm = ({ listingId }) => {
-//   const { register, handleSubmit, formState: { errors } } = useForm();
-//   const dispatch = useDispatch();
-
-//   const formSubmit = (data) => {
-//     const reviewData = {
-//       listingId: listingId,
-//     //   reservationId: reservationId,
-//     //   rating: data.rating,
-//       body: parseInt(data.body),
-//       cleanliness: parseInt(data.cleanliness),
-//       communication: parseInt(data.communication),
-//       checkin: parseInt(data.checkin),
-//       accuracy: parseInt(data.accuracy),
-//       location: parseInt(data.location),
-//       value: parseInt(data.value)
-//     };
-
-//     dispatch(createReview(reviewData));
-//   };
-
-//   const [modalIsOpen, setModalIsOpen] = useState(false);
-
-//   const openModal = () => {
-//     setModalIsOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setModalIsOpen(false);
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit(formSubmit)} className='reviewForm'>
-//         {/* <div className="form-header">
-//                 <button
-//                 className="closeButton"
-//                 onClick={closeModal}
-//                 >
-//                 X
-//                 </button>
-//                 <h2>Leave a Review</h2>
-//             </div> */}
-//         {/* <div>
-//           <label>Rating:</label>
-//           <input
-//             type="number"
-//             name="rating"
-//             min="1"
-//             max="5"
-//             {...register('rating', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.rating && <span>Please enter a rating between 1 and 5.</span>}
-//         </div> */}
-//         <div>
-//           <label>Cleanliness:</label>
-//           {/* <StarRating name="cleanliness" register={register} required onChange={(value) => handleRatingChange('rating', value)} /> */}
-//           <input
-//             type="number"
-//             name="cleanliness"
-//             min="1"
-//             max="5"
-//             {...register('cleanliness', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.cleanliness && <span>Please enter a rating between 1 and 5.</span>}
-//         </div>
-//         <div>
-//           <label>Communication:</label>
-//           <input
-//             type="number"
-//             name="communication"
-//             min="1"
-//             max="5"
-//             {...register('communication', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.communication && <span>Please enter a rating between 1 and 5.</span>}
-//         </div>
-//         <div>
-//           <label>Checkin:</label>
-//           <input
-//             type="number"
-//             name="checkin"
-//             min="1"
-//             max="5"
-//             {...register('checkin', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.checkin && <span>Please enter a rating between 1 and 5.</span>}
-//         </div>
-//         <div>
-//           <label>Accuracy:</label>
-//           <input
-//             type="number"
-//             name="accuracy"
-//             min="1"
-//             max="5"
-//             {...register('accuracy', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.accuracy && <span>Please enter a rating between 1 and 5.</span>}
-//         </div>
-//         <div>
-//           <label>Location:</label>
-//           <input
-//             type="number"
-//             name="location"
-//             min="1"
-//             max="5"
-//             {...register('location', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.location && <span>Please enter a rating between 1 and 5.</span>}
-//         </div>
-//         <div>
-//           <label>Value:</label>
-//           <input
-//             type="number"
-//             name="value"
-//             min="1"
-//             max="5"
-//             {...register('value', { required: true, min: 1, max: 5 })}
-//           />
-//           {errors.value && <span>Please enter a rating between 1 and 5.</span>}
-//         </div>
-//         <div>
-//           <label>Comments:</label>
-//           <textarea name="body" {...register('body')} />
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default ReviewsForm;
--- frontend/src/components/Reviews/ReviewsModal.js
+++ frontend/src/components/Reviews/ReviewsModal.js
@@ -5,6 +5,7 @@ import './ReviewForm.css';
 
 const ReviewsModal = ({ listingId, reservationId }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -14,15 +15,39 @@ const ReviewsModal = ({ listingId, reservationId }) => {
     setModalIsOpen(false);
   };
 
+  const handleFormSubmit = () => {
+    setFormSubmitted(true);
+    closeModal();
+  };
+
   return (
     <div>
       <button onClick={openModal}>Leave a Review</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className='review-modal'>
-        <ReviewForm listingId={listingId} reservationId={reservationId}/>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className='review-modal' ariaHideApp={false}>
+        <ReviewForm listingId={listingId} reservationId={reservationId} closeModal={handleFormSubmit}/>
         <button onClick={closeModal} className='close-review'>X</button>
       </Modal>
     </div>
   );
+
+// return (
+//     <div>
+//       <button onClick={openModal}>Leave a Review</button>
+//       <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className='review-modal'>
+//         {formSubmitted ? (
+//           <div className="form-submitted-message">
+//             <p>Review submitted successfully!</p>
+//             <button onClick={closeModal} className='close-review'>X</button>
+//           </div>
+//         ) : (
+//           <div>
+//               <ReviewForm listingId={listingId} reservationId={reservationId} closeModal={handleFormSubmit} />
+//               <button onClick={closeModal} className='close-review'>X</button>
+//           </div>
+//         )}
+//       </Modal>
+//     </div>
+//   );
 };
 
 export default ReviewsModal;
