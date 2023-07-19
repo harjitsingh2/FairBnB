73ed5a4faf7413d074f897ce5a5d4c719e6157ed
harjitsingh3 <singh.harjit09@gmail.com>
2023-07-19 12:12:51 -0700

    Display reviewer name

--- app/views/api/reviews/index.json.jbuilder
+++ app/views/api/reviews/index.json.jbuilder
@@ -7,7 +7,7 @@
 
 json.reviews @reviews do |review|
     # json.set! review.id do
-      json.extract! review, :id, :listing_id, :reservation_id, :reviewer_id, :rating, :body, :cleanliness, :communication, :checkin, :accuracy, :location, :value
+      json.extract! review, :id, :listing_id, :reservation_id, :reviewer_id, :reviewer, :rating, :body, :cleanliness, :communication, :checkin, :accuracy, :location, :value, :created_at
     # end
 end
   
--- frontend/src/components/Reviews/ReviewItem.js
+++ frontend/src/components/Reviews/ReviewItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ReviewsIndex.css';
 import ReviewIdContext from '../../context/ReviewIdContext';
 import ReviewerIdContext from '../../context/ReviewerIdContext';
 
@@ -6,20 +7,32 @@ const ReviewItem = ({ review }) => {
     const reviewId = review.id
     const reviewerId = review.reviewerId
     // debugger;
+    const reviewer = review.reviewer;
+    // console.log(reviewer);
   
+    // console.log(review.createdAt)
+
+    const reviewDate = () => {
+        let split = review.createdAt.split('-')
+        let month = split[1]
+        let year = split[0]
+
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        let displayMonth = months[month-1]
+        let displayDate = `${displayMonth} ${year}`
+        return displayDate;
+    }
+
     return (
-    <div>
+    <div className='review-details'>
         {/* <ReviewIdContext.Provider value={reviewId}>    
         <ReviewerIdContext.Provider value={reviewerId}>     */}
-            {/* <h3>Rating: {review.rating}</h3> */}
-            <p>Reviewer: {review.reviewerId}</p>
-            <p>Review: {review.body}</p>
-            {/* <p>Cleanliness: {review.cleanliness}</p>
-            <p>Communication: {review.communication}</p>
-            <p>Checkin: {review.checkin}</p>
-            <p>Accuracy: {review.accuracy}</p>
-            <p>Location: {review.location}</p>
-            <p>Value: {review.value}</p> */}
+
+            {/* <p>Reviewer: {review.reviewerId}</p> */}
+            <p id='reviewer-name'>{review.reviewer.first_name}</p>
+            <p id='review-date'>{reviewDate()}</p>
+            <p>{review.body}</p>
+
         {/* </ReviewerIdContext.Provider>
         </ReviewIdContext.Provider> */}
     </div>
